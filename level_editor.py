1e5e5c66c0c8eab9cf518352c57b45a0e7ebfe01
HarutoTanaka-go <k024g0048@g.neec.ac.jp>
2026-05-27 10:01:24 +0900

    Update level_editor.py
    okk

--- level_editor.py
+++ level_editor.py
@@ -1,17 +1,18 @@
 import bpy
 
 #ブレンダーに登録するアドオン情報
-bl_info={
-"neme":"レベルエディタ",
-"author":"satou yui",
-"version":(1,0),
-"blender":(3,3,1),
-"location":"",
-"description":"レベルエディタ",
-"warning":"",
-"wiki_url":"",
-"tracker_url":"",
-"category":"Object"
+bl_info={  
+    "name":"レベルエディタ",
+    "author": "Haruto Tanaka",
+    "version": (1,0),
+    "blender": (3, 3, 1),
+    "location": "",
+    "description": "レベルエディタ",
+    "warning":"",
+    "support": "TESTING",
+    "wiki_url":"",
+    "tracker_url":"",
+    "category":"Object"
 }
 
 #アドオン有効化時コールバック
